2d306dc390634f5e0424069f988956f209e0518f
M-Osama96 <mohamedosama3466@gmail.com>
2025-11-13 00:17:47 +0200

    end 12-11

--- js/main.js
+++ js/main.js
@@ -49,68 +49,14 @@ if (document.querySelector(".tabs") !== null) {
     console.log(button);
   });
 }
-
-var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-if (document.querySelectorAll(".clients-cont") !== null) {
-  let elementsArray = document.querySelectorAll(".clients-cont");
-  window.addEventListener("scroll", fadeIn);
-  function fadeIn() {
-    for (var i = 0; i < elementsArray.length; i++) {
-      var elem = elementsArray[i];
-      var distInView =
-        elem.getBoundingClientRect().top - window.innerHeight + 0;
-      console.log(distInView);
-      if (distInView < 0) {
-        elem.classList.add("inView");
-      } else {
-        elem.classList.remove("inView");
-      }
+if (document.querySelector(".myHeader") !== null) {
+  const header = document.querySelector(".myHeader");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      // لما تنزل شوية
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
     }
-  }
-  fadeIn();
-}
-
-if (document.querySelectorAll(".accordion-header") !== null) {
-  const accordionHeaders = document.querySelectorAll(".accordion-header");
-
-  accordionHeaders.forEach((header) => {
-    header.addEventListener("click", () => {
-      const item = header.parentElement;
-      const openItem = document.querySelector(".accordion-item.active-a");
-
-      // لو فيه عنصر مفتوح غير الحالي → يقفله
-      if (openItem && openItem !== item) {
-        openItem.classList.remove("active-a");
-      }
-
-      // يقلب حالة العنصر الحالي (فتح / غلق)
-      item.classList.toggle("active-a");
-    });
   });
 }
-// if (document.querySelectorAll("mySelect") !== null) {
-//   const select = document.getElementById("mySelect");
-//   select.addEventListener("change", () => {
-//     if (select.value) select.classList.add("selected");
-//   });
-// }
-
-const header = document.getElementById("myHeader");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    // لما تنزل شوية
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
-});
